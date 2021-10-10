62b13de3ae8a5a76c8b7ece3d9c6bdcd7f80a9c5
luqas <luqas.business@gmail.com>
2021-10-10 20:10:23 +0200

    10/10/2021

---
+++ main.py
@@ -0,0 +1,13 @@
+import string
+import random
+
+chars = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+def generate_random_password():
+	l = int(9)
+	random.shuffle(chars)
+	password = []
+	for i in range(l):
+  	  password.append(random.choice(chars))
+	random.shuffle(password)
+	print("".join(password))
+generate_random_password()
